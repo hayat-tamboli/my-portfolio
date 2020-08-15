27c21d7f7a5b2ae118d54cb6659bbe2d0d7522c7
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-08-15 14:56:36 +0530

    🍔❌ initialise the nav on mobile

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="header">
     <span class="logo">
-      <a href="#home">Hayat Tamboli</a>
+      <a href="#home">HAYAT</a>
+      <button
+        class="menu"
+        onclick="this.classList.toggle('opened');this.setAttribute('aria-expanded', this.classList.contains('opened'))"
+        aria-label="Main Menu"
+      >
+        <svg width="40" height="40" viewBox="0 0 100 100">
+          <path
+            class="line line1"
+            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+          />
+          <path class="line line2" d="M 20,50 H 80" />
+          <path
+            class="line line3"
+            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+          />
+        </svg>
+      </button>
     </span>
     <ul class="links">
       <li>
@@ -115,10 +132,70 @@ $easeOutBack: cubic-bezier(0.175, 0.885, 0.32, 1.275);
     }
   }
 }
+.menu {
+  display: none;
+}
+
 @media only screen and (max-width: 900px) {
+  button {
+    &:hover {
+      transform: translateY(0px);
+      box-shadow: 0px 0px 0px rgb(0, 0, 0);
+    }
+  }
+  .menu {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    padding: 0;
+  }
+  .line {
+    fill: none;
+    stroke: black;
+    stroke-width: 6;
+    transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
+      stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+  .line1 {
+    stroke-dasharray: 60 207;
+    stroke-width: 6;
+  }
+  .line2 {
+    stroke-dasharray: 60 60;
+    stroke-width: 6;
+  }
+  .line3 {
+    stroke-dasharray: 60 207;
+    stroke-width: 6;
+  }
+  .opened {
+    .line1 {
+      stroke-dasharray: 90 207;
+      stroke-dashoffset: -134;
+      stroke-width: 6;
+    }
+    .line2 {
+      stroke-dasharray: 1 60;
+      stroke-dashoffset: -30;
+      stroke-width: 6;
+    }
+    .line3 {
+      stroke-dasharray: 90 207;
+      stroke-dashoffset: -134;
+      stroke-width: 6;
+    }
+  }
+
   .header {
     max-width: 100vw;
-    justify-content: center;
+    display: inline-block;
+    .logo {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 2rem;
+    }
     .links {
       display: none;
     }
